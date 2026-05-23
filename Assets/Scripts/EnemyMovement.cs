705ede48729b4c099fc06d79a9e41a9efb3e944c
sliverbird5337 <chanhosinp@gmail.com>
2026-05-23 18:38:26 +0900

    enemyDestroy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -69,4 +69,10 @@ public class EnemyMovement : MonoBehaviour
     {
         Debug.Log("waiting");
     }
+
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject);
+        Debug.Log("enemy destoryed");
+    }
 }
