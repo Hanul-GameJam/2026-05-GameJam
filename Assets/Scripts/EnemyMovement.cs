8b045feb9e16cae8e1292a10a2d99693f849af9c
sliverbird5337 <chanhosinp@gmail.com>
2026-05-23 17:39:57 +0900

    EnemyMovement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Numerics;
+using System.Runtime.Serialization;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{   
+
+    private Rigidbody2D rb;
+
+    [SerializeField] public float EnemySpeed = 5f;
+    // [SerializeField] public GameObject.Transform.Position EnemyPosition;
+    [SerializeField] public float turnInterval = 3f;  // 3 sec
+    private float timer = 0f;
+    [SerializeField] public bool isEnemyFacingLeft = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // UnityEngine.Vector3 move = new UnityEngine.Vector3(1, 0, 0);
+        timer += Time.deltaTime;
+        if (timer >= turnInterval)
+        {
+            timer = 0.0f;
+            Wait();
+            isEnemyFacingLeft = !isEnemyFacingLeft;
+        }
+
+        EnemyRoaming(isEnemyFacingLeft);
+
+    
+        //transform.Translate(move * EnemySpeed);
+        //transform.position = ;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player health decreased");
+        }
+    }
+
+    private void EnemyRoaming(bool isEnemyFacingLeft)
+    {   
+        //rb.AddForce(UnityEngine.Vector2.right * EnemySpeed, ForceMode2D.Impulse);
+        if (!isEnemyFacingLeft)
+        {
+            transform.position += UnityEngine.Vector3.right * EnemySpeed * Time.deltaTime;
+        }
+        else if (isEnemyFacingLeft)
+        {
+            transform.position += UnityEngine.Vector3.left * EnemySpeed * Time.deltaTime;
+        }
+        
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10.0f);
+        Debug.Log("waited");
+    }
+}
