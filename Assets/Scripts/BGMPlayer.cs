f97944fcb5b5d8f4a9f0f42e3d31de93da514632
minkyole <minkyole@student.42seoul.kr>
2026-05-24 02:24:47 +0900

    feat: MainMenu, 일시정지

---
+++ Assets/Scripts/BGMPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour
+{
+    // 싱글톤 인스턴스
+    public static BGMPlayer instance;
+
+    void Awake()
+    {
+        // 이미 씬에 다른 BGM_Player가 있다면, 새로 만들어진 나는 파괴한다 (중복 방지)
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 내가 최초라면 나를 등록하고 씬 전환 시 파괴되지 않게 설정
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.Net.Mime.MediaTypeNames;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPlayClick() => SceneManager.LoadScene("Level_1-1");
+    public void OnExitClick()
+    {
+        // 2. 실제 완성된 게임에서는 정상적으로 프로그램이 꺼집니다.
+        UnityEngine.Application.Quit();
+
+        // 💡 (선택 꿀팁) 유니티 에디터에서도 끄고 싶다면 아래 코드를 추가하세요!
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
